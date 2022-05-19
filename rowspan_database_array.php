bf87b0915c0d40ee340c80b7037a9fa3f7f4c2e6
MD FAHAMIDUL HAQUE <49103044+Aoyon123@users.noreply.github.com>
2022-05-19 18:52:09 +0600

    Add files via upload

--- rowspan_database_array.php
+++ rowspan_database_array.php
@@ -1,98 +1,137 @@
-
 <!DOCTYPE html>
-<?php
-$conn = mysqli_connect("localhost", "root", "", "crud18");
+<html>
+    <head>
+        <title>Array</title>
+    </head>
+    <body>
+        <?php
+//        require_once 'header.php';
+        require_once 'db.php';
 
-if (!$conn) {
-    echo "Database Connection Failed";
-}
+        // create array from database
+        // get division
+//        $division = array();
+//        $division_sql = "SELECT * FROM division";
+//        $division_result = mysqli_query($conn,$division_sql);
+//        while ($row = mysqli_fetch_assoc($division_result)) {
+//            
+//            $division[$row['id']] = $row['name'];
+//        }
+        //get user detail
+        $user = array();
+        $user_sql = "SELECT users.* , division.name as division_name, district.name as district_name, thana.name as thana_name
 
-$getQuery = "SELECT * FROM tableuser";
-$result = mysqli_query($conn, $getQuery);
-?>
-<body>
-    <?php
-    while ($row = mysqli_fetch_array($result)) {
+            FROM users
 
-        $userdata[$row['division_id']][$row['id']]['firstName'] = $row['firstName'];
-        $userdata[$row['division_id']][$row['id']]['lastName'] = $row['lastName'];
-        $userdata[$row['division_id']][$row['id']]['email'] = $row['email'];
-    }
+            LEFT JOIN division ON division.id =  users.division_id
 
-    echo '<pre>';
-    print_r($userdata);
+            LEFT JOIN district ON district.id = users.district_id
 
-    $divisionArr = array();
-    $div_sql = "SELECT * FROM division";
-    $getdata = mysqli_query($conn, $div_sql);
+            LEFT JOIN thana ON thana.id = users.thana_id";
 
-    while ($row = mysqli_fetch_assoc($getdata)) {
-        $divisionArr[$row['id']] = $row['name'];
-    }
+        $user_result = mysqli_query($conn,
+                $user_sql);
+        while ($row = mysqli_fetch_assoc($user_result)) {
+            $user[$row['division_id']]['division_name'] = $row['division_name'];
+            $user[$row['division_id']]['district'][$row['district_id']]['district_name'] = $row['district_name'];
+            $user[$row['division_id']]['district'][$row['district_id']]['user'][$row['id']]['first_name'] = $row['fname'];
+            $user[$row['division_id']]['district'][$row['district_id']]['user'][$row['id']]['last_name'] = $row['lname'];
+            $user[$row['division_id']]['district'][$row['district_id']]['user'][$row['id']]['username'] = $row['username'];
+        }
 
-    echo "<pre>";
-    print_r($divisionArr);
-    ?>
-
-    <table border="3" cellpadding="10" cellspacing="3">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Division</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $rowSpan = 0;
-            foreach ($userdata as $divId => $userInfo) {
-                //echo "<br>";
-                //print_r($userInfo);
-                $rowSpan = count($userInfo);
-                $rowCounter = 0;
-                foreach ($userInfo as $userId => $value) {
-                    //print_r($value);
-                    $fname = $value['firstName'];
-                    $lname = $value['lastName'];
-                    $email = $value['email'];
-                    ?>
-                    <tr>
-                        <td><?php echo $divId; ?></td>
-                        <?php
-                        if ($rowCounter == 0) {
-                            ?>
-                            <td rowspan="<?php echo $rowSpan; ?>"><?php echo $divisionArr[$divId]; ?></td>
-                            <?php
+        $rowspanArr = [
+];
+        if (!empty($user)) {
+            foreach ($user as
+                    $divId =>
+                    $divInfo) {
+                if (!empty($divInfo['district'])) {
+                    foreach ($divInfo['district'] as
+                            $disId =>
+                            $disInfo) {
+                        if (!empty($disInfo['user'])) {
+                            foreach ($disInfo['user'] as
+                                    $uId =>
+                                    $uInfo) {
+                                $rowspanArr['dis'][$divId][$disId] = !empty($rowspanArr['dis'][$divId][$disId]) ? $rowspanArr['dis'][$divId][$disId] : 0;
+                                $rowspanArr['dis'][$divId][$disId] += 1;
+                                
+                                $rowspanArr['div'][$divId] = !empty($rowspanArr['div'][$divId]) ? $rowspanArr['div'][$divId] : 0;
+                                $rowspanArr['div'][$divId] += 1;
+                            }
                         }
-                        ?>
-                        <td><?php echo $fname; ?></td>
-                        <td><?php echo $lname; ?></td>
-                        <td><?php echo $email; ?></td>
-
-                    </tr>
-                    <?php
-                    $rowCounter++;
+                    }
                 }
             }
-            exit;
-            ?>
-            </tr>
-        </tbody>
-    </table>
+        }
 
+        echo "<pre>";
+//        print_r($rowspanArr);
+        print_r($user);
+        echo "</pre>";
+        ?>
 
-
-
-
-
-    <div class="tm-25">
-        <?php include 'footer.php'; ?>
-    </div>
-</div>
-
-
-</body>
+        <!-- show data in table -->
+        <table border="1" cellpadding="0" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Division</th>
+                    <th>District</th>
+                    <th>Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php
+                    if (!empty($user)) {
+                        foreach ($user as $divId =>$divInfo) {
+                            ?>
+                        <tr>
+                            <td rowspan="<?php echo!empty($rowspanArr['div'][$divId]) ? $rowspanArr['div'][$divId] : 1; ?>"><?php echo!empty($divInfo['division_name']) ? $divInfo['division_name'] : 0; ?> </td>
+                            <?php
+                            if (!empty($divInfo['district'])) {
+                                $i = 0;
+                                foreach ($divInfo['district'] as
+                                        $disId =>
+                                        $disInfo) {
+                                    if ($i > 0) {
+                                        echo "<tr>";
+                                    }
+                                    ?>
+                                    <td rowspan="<?php echo!empty($rowspanArr['dis'][$divId][$disId]) ? $rowspanArr['dis'][$divId][$disId] : 1; ?>"><?php echo!empty($disInfo['district_name']) ? $disInfo['district_name'] : 0; ?> </td>
+                                    <?php
+                                    if (!empty($disInfo['user'])) {
+                                        $j = 0;
+                                        foreach ($disInfo['user'] as $uId =>$uInfo) {
+                                            if ($j > 0) {
+                                                echo "<tr>";
+                                            }
+                                            $uName = $uInfo['first_name'] . " " . $uInfo['last_name'];
+                                            ?>
+                                            <td><?php echo!empty($uName) ? $uName : 0; ?></td>
+                                            <td><?php echo!empty($uInfo['username']) ? $uInfo['username'] : 0; ?></td>
+                                            <?php
+                                            if ($j < ($rowspanArr['dis'][$divId][$disId]) - 1) {
+                                                echo "</tr>";
+                                            }
+                                            $j++;
+                                        }
+                                        ?>
+                                    <?php
+                                    if ($i < ($rowspanArr['div'][$divId]) - 1) {
+                                        echo "</tr>";
+                                    }
+                                    $i++;
+                                }
+                                ?>
+                                </tr>
+                <?php
+            }
+        }
+    }
+}
+?>    
+            </tbody>
+        </table>
+    </body>
 </html>
